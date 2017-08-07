40d2fa60b3a5f0f68cca203a6ecac15befb7f5c1
Arya H <aryaht23@gmail.com>
2017-08-06 23:13:07 -0500

    pi1 fix

--- pi_integral_trapizoid.c
+++ pi_integral_trapizoid.c
@@ -2,13 +2,12 @@
 #include <stdlib.h>
 #include <math.h>
 #include "mpi.h"
-// created by Arya HajiTaheri
 double f(double x){
-        return 4/(1+x*x); // our exact function to find pi
+        return 4/(1+x*x);
 }
 
 int main(int argc, char  *argv[]) {
-	int size, rank, n0, nstep;
+	int size, rank, i0, istep;
 	double start_time, end_time, elapsed_time;
 
 	MPI_Init(&argc, &argv);
@@ -22,38 +21,39 @@ int main(int argc, char  *argv[]) {
 	double a = 1, b = 0, n = 50000, i,
 		h, sum1, sum, summ, yn, y0;
 
+	MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-	n0 = (rank * 2) + 1;
-	nstep = size * 2;
+	i0 = (rank * 2) + 1;
+	istep = size * 2;
 	start_time = MPI_Wtime();
 	if (rank == 0) { //first node
-		printf("Using %d nodes:", size);
+		printf("Using %d nodes:\n", size);
 		h = (b - a) / n;;
 		y0 = f(a + 0 * h);
 		yn = f(a + n*h);
-		// trapezoidal method
-		for (i = 1; i < n; i++) {
+
+		for (i = rank; i <= n; i+= size) {
 			sum1 = sum1 + f(a + i*h);
 		}
 		sum = (h / 2)*(y0 + yn + 2 * sum1);
-		MPI_Reduce(&sum, &summ, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+		MPI_Reduce(&sum, &summ, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 		end_time = MPI_Wtime();
 
-		printf("Answer : %.15lf,\nPI = %.15lf", sum, M_PI);
-		printf("\n%.15lf", fabs(M_PI + sum));
+		printf("Answer : %.15lf,\nPI = %.15lf\n", fabs(summ), M_PI);
+		printf("\n%.15lf%%\n", fabs(M_PI + summ)/M_PI*100);
 		elapsed_time = end_time - start_time;
-		printf("It took %lf seconds to estimate pi", elapsed_time);
+		printf("It took %lf seconds to estimate pi\n", elapsed_time);
 	}
 	if (rank > 0) { //rest of the tasks
 		h = (b - a) / n;;
 		y0 = f(a + 0 * h);
 		yn = f(a + n*h);
 
-		for (i = 1; i < n; i++) {
+		for(i = rank; i <= n; i+= size ) {
 			sum1 = sum1 + f(a + i*h);
 		}
 		sum = (h / 2)*(y0 + yn + 2 * sum1);
-		MPI_Reduce(&sum, &summ, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+		MPI_Reduce(&sum, &summ, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
 
 	}
 
